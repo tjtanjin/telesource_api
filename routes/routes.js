419a155b369a439f0dc957ae74beefe176e92e89
tjtanjin <cjtanjin@gmail.com>
2021-01-05 04:41:02 -0500

    fix: add missing .env file in .gitignore. update command for js-slang

--- routes/routes.js
+++ routes/routes.js
@@ -13,14 +13,14 @@ router.post('/', async (req, res) => {
   let result = ""
   const userid = req.body.userid;
   const sourceVersion = req.body.version;
-  const command = "jq -r '.code_snippet' ../telesourcebot/userinfo/" + userid + ".json" + ' | node js-slang/dist/repl/repl.js ' + sourceVersion
+  const command = "jq -r '.code_snippet' ../telesourcebot/userinfo/" + userid + ".json" + ' | node js-slang/dist/repl/repl.js -c ' + sourceVersion
   const { exec } = require('child_process');
   exec(command, (error, stdout, stderr) => {
     if (error) {
       console.error(`exec error: ${error}`);
       return;
     }
-    const split = stdout.split("\n")
+    const split = stdout.split("\n");
     if (split[1].includes("> Error:")) {
       result = split[split.length-3].slice(2)
     } else {
